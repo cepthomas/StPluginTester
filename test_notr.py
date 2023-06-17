c5426be8c09b56c1cf82ee0607714b7d8843cbcb
cepthomas <cepthomas@gmail.com>
2023-06-17 18:03:57 -0500

    WIP.

--- test_notr.py
+++ test_notr.py
@@ -6,7 +6,6 @@ from unittest.mock import MagicMock
 import sublime
 import sublime_plugin
 
-# TODO This supports execution in VS or from command line but doesn't support intellisense.
 from Notr import notr, table
 
 
@@ -20,23 +19,13 @@ class TestNotr(unittest.TestCase):
         files_path = os.path.join(sublime.packages_path(), 'Notr', 'files')
         mock_settings = {
             "visual_line_length": 100,
-            "user_hl": [["2DO", "things"], ["user"], ["dynamic"], ["and_a"], ["and_b"], ["and_c"]],
+            "user_hl": [["2DO", "things"], ["user", "dynamic"], ["and_a", "and_b", "and_c"]],
             "user_hl_whole_word": True,
         }
         mock_settings["notr_paths"] = [files_path]
         mock_settings["notr_index"] = os.path.join(files_path, 'test-index.ntr')
         sublime.load_settings = MagicMock(return_value=mock_settings)
 
-        # Init internals.
-        self.view = sublime.View(600)
-        sel = sublime.Selection(self.view.id())
-        sel.add(sublime.Region(10, 20, 101))
-        self.view.sel = MagicMock(return_value=sel)
-
-        # Mock syntax.
-        syntax = sublime.Syntax('', 'Notr', False, '')
-        syntax.name = MagicMock(return_value='Notr')
-        self.view.syntax = MagicMock(return_value=syntax)
 
     def tearDown(self):
         pass
@@ -44,10 +33,23 @@ class TestNotr(unittest.TestCase):
     #@unittest.skip
     def test_TableFit(self):
 
+        #'|State |    Size    |Color|',
+        #'|   ME   |34|   Red     |',28
+        #'|  IA   |  31    |Blue     |',54
+        #'| CO  | 15  | Brown    |',83
+        #'| NY     | 4  | Yellow    |',108
+        #'| TX| 2  | Green    |',136
+        #'| WY   | 45        | White    |',158-189
+        #{ "scope": "meta.table", "background": "lightblue" },
+        #{ "scope": "meta.table.header", "background": "deepskyblue" },
+
+
         test_text = [
-            'not-table',
-            'not-table',
-            'not-table',
+            'notable 1',
+            'notable 2',
+            'notable 3',
+            'notable 4',
+            'notable 5',
             '|State |    Size    |Color|',
             '|   ME   |34|   Red     |',
             '|  IA   |  31    |Blue     |',
@@ -55,28 +57,59 @@ class TestNotr(unittest.TestCase):
             '| NY     | 4  | Yellow    |',
             '| TX| 2  | Green    |',
             '| WY   | 45        | White    |',
-            'not-table',
-            'not-table',
-            'not-table',
+            'notable 6',
+            'notable 7',
+            'notable 8',
+            'notable 9',
             ]
 
-        edit = sublime.Edit('test')
-        cmd = table.TableFitCommand(self.view)
-        cmd.run(edit)
+        view = sublime.View(600)
+        view.insert(None, 0, '\n'.join(test_text))
+        sel = sublime.Selection(view.id())
+        #sel.add(sublime.Region(25, 29)) # before table
+        sel.add(sublime.Region(73, 77)) # in table
+        view.sel = MagicMock(return_value=sel)
+
+        def scope_name(*args, **kwargs):
+            pos = args[0]
+            if pos >= 28 and pos <= 53:
+                return 'text.notr meta.table.header'
+            elif pos >= 54 and pos <= 189:
+                return 'text.notr meta.table'
+            else:
+                return 'text.notr'
+            pass
+
+        view.scope_name = MagicMock(side_effect=scope_name)
+
+        #edit = sublime.Edit('test')
+        cmd = table.TableFitCommand(view)
+        cmd.run(None)
 
 
 
-
-    @unittest.skip
+    #@unittest.skip
     def test_on_init(self):
+        ''' Tests the ntr file parsing. '''
+
+        view = sublime.View(601)
+        sel = sublime.Selection(view.id())
+        sel.add(sublime.Region(10, 20, 101))
+        view.sel = MagicMock(return_value=sel)
+
+        # Mock syntax.
+        syntax = sublime.Syntax('', 'Notr', False, '')
+        #syntax.name = MagicMock(return_value='Notr')
+        view.syntax = MagicMock(return_value=syntax)
+
         window = sublime.Window(500)
-        self.view._window = MagicMock(return_value=window)
+        view._window = MagicMock(return_value=window)
 
         vnew = sublime.View(501)
         window.new_file = MagicMock(return_value = vnew)
 
         evt = notr.NotrEvent()
-        evt.on_init([self.view])
+        evt.on_init([view])
 
         self.assertEqual(len(notr._tags), 7)
         self.assertEqual(len(notr._links), 6)
@@ -87,19 +120,19 @@ class TestNotr(unittest.TestCase):
     @unittest.skip
     def test_GotoRef(self):
         edit = sublime.Edit('test')
-        cmd = notr.NotrGotoRefCommand(self.view)
+        cmd = notr.NotrGotoRefCommand(view)
         cmd.run(edit)
 
     @unittest.skip
     def test_GotoSection(self):
         edit = sublime.Edit('test')
-        cmd = notr.NotrGotoSectionCommand(self.view)
+        cmd = notr.NotrGotoSectionCommand(view)
         cmd.run(edit)
 
     @unittest.skip
     def test_InsertRef(self):
         edit = sublime.Edit('test')
-        cmd = notr.NotrInsertRefCommand(self.view)
+        cmd = notr.NotrInsertRefCommand(view)
         cmd.run(edit)
 
 
